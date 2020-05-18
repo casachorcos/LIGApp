38f81cbe4733584007eada3d8f4f8ba86fd0346c
Jachi97 <061942223x@uma.es>
2020-05-18 12:21:12 +0200

    Create Jornada.java

---
+++ src/LEJ/Jornada.java
@@ -0,0 +1,20 @@
+package LEJ;
+
+import java.util.*;
+
+public class Jornada {
+	
+	private date fechaInicio;
+	private date fechaFin;
+
+	
+	public Jornada() {
+		
+	}
+	
+	public Jornada(fIni,fFin) {
+		
+		fIni = fechaInicio;
+		fFin = fechaFin;
+	}
+}
